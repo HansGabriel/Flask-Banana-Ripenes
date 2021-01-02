d96f39272f087c23b9ead0c15c45155469935c30
Hans Daduya <hansdaduya@yahoo.com>
2021-01-02 21:36:41 +0800

    :recycle: File submission partially working
    File submission partially working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 from algorithms.classify import classifyBanana
 
 app = Flask(__name__)
@@ -7,5 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # result = classifyBanana('static/images/banaa.png')
-    result = classifyBanana('static/images/banaa.png')
-    return 'Hello, Hans!'
+    return render_template('main.html')
+
+@app.route('/uploadajax', methods=['POST'])
+def upldfile():
+    if request.method == 'POST':
+        test = {"test": "test", "name": "name"}
+        files = request.files['file']
+        result = classifyBanana('static/images/banaa.png')
+        return jsonify(test)
+        # if files and allowed_file(files.filename):
+        #     filename = secure_filename(files.filename)
+        #     app.logger.info('FileName: ' + filename)
+        #     updir = os.path.join(basedir, 'upload/')
+        #     files.save(os.path.join(updir, filename))
+        #     file_size = os.path.getsize(os.path.join(updir, filename))
+        #     return jsonify(name=filename, size=file_size)
---
+++ static/js/app.js
@@ -0,0 +1,24 @@
+console.log('javascript starting')
+
+$(function() {
+    $('#submit').click(function() {
+        event.preventDefault();
+        var form_data = new FormData($('#uploadform')[0]);
+        $.ajax({
+            type: 'POST',
+            url: '/uploadajax',
+            data: form_data,
+            contentType: false,
+            processData: false,
+            dataType: 'json'
+        }).done(function(data, textStatus, jqXHR){
+            console.log(data);
+            console.log(textStatus);
+            console.log(jqXHR);
+            console.log('Success!');
+            $("#resultFilename").text(data['name']);
+        }).fail(function(data){
+            alert('error!');
+        });
+    });
+}); 
