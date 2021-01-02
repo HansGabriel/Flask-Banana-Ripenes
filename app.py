82ac79aa9f8c0759a823b1abd3039d1d0c668fb2
Hans Daduya <hansdaduya@yahoo.com>
2021-01-02 21:51:13 +0800

    :building_construction: File storage now working
    File storage now working

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 from algorithms.classify import classifyBanana
 
+from werkzeug.utils import secure_filename
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    # result = classifyBanana('static/images/banaa.png')
     return render_template('main.html')
 
 @app.route('/uploadajax', methods=['POST'])
@@ -14,12 +18,10 @@ def upldfile():
     if request.method == 'POST':
         test = {"test": "test", "name": "name"}
         files = request.files['file']
-        result = classifyBanana('static/images/banaa.png')
-        return jsonify(test)
-        # if files and allowed_file(files.filename):
-        #     filename = secure_filename(files.filename)
-        #     app.logger.info('FileName: ' + filename)
-        #     updir = os.path.join(basedir, 'upload/')
-        #     files.save(os.path.join(updir, filename))
-        #     file_size = os.path.getsize(os.path.join(updir, filename))
-        #     return jsonify(name=filename, size=file_size)
+        # result = classifyBanana('static/images/banaa.png')
+        filename = secure_filename(files.filename)
+        app.logger.info('FileName: ' + filename)
+        updir = os.path.join(basedir, 'upload/')
+        files.save(os.path.join(updir, filename))
+        file_size = os.path.getsize(os.path.join(updir, filename))
+        return jsonify(name=filename, size=file_size)
